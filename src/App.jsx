12efc5d0bcbbe8aec5f294dd73427c8a21cc8aa2
pratibha <pratibha.email.com>
2026-05-28 12:05:29 -0400

    added button for each feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,31 @@ import SearchFilms from "./searchFilms"
 import PaginationProductList from './pagination'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState("films");
 
   return (
-    <>
-    
- <PokemonCard></PokemonCard>
-  <SearchFilms></SearchFilms>
-  <PaginationProductList></PaginationProductList>
- <PokemonList></PokemonList>
 
-    </>
+    <div>
+      <header>
+        <h1>My App</h1>
+        <nav>
+          <button onClick={() => setActiveTab("films")} style={{color: "green"}}>Search Flims</button>
+            <button onClick={() => setActiveTab("products")} style={{color: "blue"}}>Product List</button>
+              <button onClick={() => setActiveTab("pokemonCards")} style={{color: "red"}}>Pokemon Card</button>
+                <button onClick={() => setActiveTab("pokemon")} style={{color: "purple"}}>Pokemon List</button>
+         
+        </nav>
+      </header>
+   
+    <main>
+   
+    {activeTab === "films" &&  <SearchFilms></SearchFilms>}
+    {activeTab === "products" &&  <PaginationProductList></PaginationProductList> }
+     {activeTab === "pokemonCards" &&   <PokemonList></PokemonList> }
+      {activeTab === "pokemon" &&   <PokemonCard></PokemonCard> }
+       </main>
+    </div>
+    
   )
 }
 
